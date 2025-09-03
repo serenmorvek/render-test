9f24b4b32a9c0c41835875b212f94d7f28ba9bf0
Seren Morvek <229083256+serenmorvek@users.noreply.github.com>
2025-09-03 13:21:22 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
-
-app.use(express.static('dist'))
-app.use(cors())
 
 let notes = [
     {
@@ -31,8 +27,9 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
-app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
+app.use(express.json())
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
